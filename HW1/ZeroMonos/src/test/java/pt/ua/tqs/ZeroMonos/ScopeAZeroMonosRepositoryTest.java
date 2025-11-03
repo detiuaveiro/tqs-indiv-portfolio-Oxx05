1005b47bc6bca33b3e4b59153d24abc6207fa0c0
Bernardo Martins <bernardoam05@gmail.com>
2025-11-03 23:42:36 +0000

    ajuste de CI #11

--- HW1/ZeroMonos/src/test/java/pt/ua/tqs/ZeroMonos/ScopeAZeroMonosRepositoryTest.java
+++ HW1/ZeroMonos/src/test/java/pt/ua/tqs/ZeroMonos/ScopeAZeroMonosRepositoryTest.java
@@ -3,7 +3,6 @@ package pt.ua.tqs.ZeroMonos;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import pt.ua.tqs.ZeroMonos.data.ZeroMonosRepository;
--- HW1/ZeroMonos/src/test/java/pt/ua/tqs/ZeroMonos/ScopeCRequestControllerWithMockServiceTest.java
+++ HW1/ZeroMonos/src/test/java/pt/ua/tqs/ZeroMonos/ScopeCRequestControllerWithMockServiceTest.java
@@ -3,7 +3,6 @@ package pt.ua.tqs.ZeroMonos;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
